8eafbe4828a8efbf2c8f68437262d787ffe25b6d
Oli <Oli@Oli-POLARity>
2011-06-29 14:57:22 +0100

    Forgot Undo Class

---
+++ src/uk/co/oliwali/DataLog/Undo.java
@@ -0,0 +1,31 @@
+package uk.co.oliwali.DataLog;
+
+import java.util.List;
+
+import org.bukkit.block.BlockState;
+
+import uk.co.oliwali.DataLog.util.Util;
+
+public class Undo implements Runnable {
+	
+	public PlayerSession session = null;
+	
+	public Undo(PlayerSession session) {
+		this.session = session;
+	}
+	
+	public void run() {
+		List<BlockState> results = session.getRollbackUndo();
+		if (results == null || results.size() == 0) {
+		Util.sendMessage(session.getSender(), "&cNo rollbacks to undo");
+		return;
+		}
+		Util.sendMessage(session.getSender(), "&cUndoing rollback (&7" + results.size() + " action(s)&c)");
+		for (BlockState block : results.toArray(new BlockState[0]))
+		block.update(true);
+		Util.sendMessage(session.getSender(), "&cUndo complete");
+		session.setRollbackUndo(null);
+		session.setRollbackResults(null);
+	}
+	
+}
