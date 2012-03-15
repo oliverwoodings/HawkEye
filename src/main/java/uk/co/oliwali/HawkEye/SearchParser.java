e3db23524741c1ca3221ed2bb8f93306ad796ace
GoalieGuy6 <l.mikelonis@yahoo.com>
2012-03-15 08:50:44 -0400

    Respect max-radius option in config

--- src/main/java/uk/co/oliwali/HawkEye/SearchParser.java
+++ src/main/java/uk/co/oliwali/HawkEye/SearchParser.java
@@ -9,6 +9,7 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
 
+import uk.co.oliwali.HawkEye.util.Config;
 import uk.co.oliwali.HawkEye.util.Permission;
 import uk.co.oliwali.HawkEye.util.Util;
 
@@ -17,7 +18,7 @@ import uk.co.oliwali.HawkEye.util.Util;
  * @author oliverw92
  */
 public class SearchParser {
-	
+
 	public Player player = null;
 	public String[] players = null;
 	public Vector loc = null;
@@ -29,56 +30,56 @@ public class SearchParser {
 	public String dateFrom = null;
 	public String dateTo = null;
 	public String[] filters = null;
-	
+
 	public SearchParser() { }
-	
+
 	public SearchParser(Player player) {
 		this.player = player;
 	}
-	
+
 	public SearchParser(Player player, int radius) {
 		this.player = player;
 		this.radius = radius;
 		parseLocations();
 	}
-	
+
 	public SearchParser(Player player, List<String> args) throws IllegalArgumentException {
 		this.player = player;
-		
+
 		String lastParam = "";
 		boolean paramSet = false;
-		
+
 		for (int i = 0; i < args.size(); i++) {
 			String arg = args.get(i);
 			if (arg.isEmpty()) continue;
-			
+
 			if (!paramSet) {
 				if (arg.length() < 2)
 					throw new IllegalArgumentException("Invalid argument format: &7" + arg);
 				if (!arg.substring(1,2).equals(":"))
 					throw new IllegalArgumentException("Invalid argument format: &7" + arg);
-				
+
 				lastParam = arg.substring(0,1).toLowerCase();
 				paramSet = true;
-				
+
 				if (arg.length() == 2) {
 					if (i == (args.size() - 1)) // No values specified
 						throw new IllegalArgumentException("Invalid argument format: &7" + arg);
 					else // User put a space between the colon and value
 						continue;
 				}
-				
+
 				// Get values out of argument
 				arg = arg.substring(2);
 			}
-			
+
 			if (paramSet) {
 				if (arg.isEmpty()) {
 					throw new IllegalArgumentException("Invalid argument format: &7" + lastParam + ":");
 				}
-				
+
 				String[] values = arg.split(",");
-				
+
 				// Players
 				if (lastParam.equals("p")) players = values;
 				// Worlds
@@ -106,7 +107,7 @@ public class SearchParser {
 				}
 				// Location
 				else if (lastParam.equals("l")) {
-					if (values[0].equalsIgnoreCase("here")) 
+					if (values[0].equalsIgnoreCase("here"))
 						loc = player.getLocation().toVector();
 					else {
 						loc = new Vector();
@@ -119,10 +120,11 @@ public class SearchParser {
 				else if (lastParam.equals("r")) {
 					if (!Util.isInteger(values[0])) throw new IllegalArgumentException("Invalid radius supplied: &7" + values[0]);
 					radius = Integer.parseInt(values[0]);
+					if (Config.MaxRadius != 0 && radius > Config.MaxRadius) throw new IllegalArgumentException("Radius too large, max allowed: &7" + Config.MaxRadius);
 				}
 				//Time
 				else if (lastParam.equals("t")) {
-					
+
 					int type = 2;
 					for (int j = 0; j < arg.length(); j++) {
 						String c = arg.substring(j, j+1);
@@ -133,16 +135,16 @@ public class SearchParser {
 								type = 1;
 						}
 					}
-					
+
 					//If the time is in the format '0w0d0h0m0s'
 					if (type == 0) {
-						
+
 						int weeks = 0;
 						int days = 0;
 						int hours = 0;
 						int mins = 0;
 						int secs = 0;
-						
+
 						String nums = "";
 						for (int j = 0; j < values[0].length(); j++) {
 							String c = values[0].substring(j, j+1);
@@ -159,7 +161,7 @@ public class SearchParser {
 							else throw new IllegalArgumentException("Invalid time measurement: &7" + c);
 							nums = "";
 						}
-						
+
 						Calendar cal = Calendar.getInstance();
 						cal.add(Calendar.WEEK_OF_YEAR, -1 * weeks);
 						cal.add(Calendar.DAY_OF_MONTH, -1 * days);
@@ -168,7 +170,7 @@ public class SearchParser {
 						cal.add(Calendar.SECOND, -1 * secs);
 						SimpleDateFormat form = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 						dateFrom = form.format(cal.getTime());
-						
+
 					}
 					//If the time is in the format 'yyyy-MM-dd HH:mm:ss'
 					else if (type == 1) {
@@ -184,37 +186,40 @@ public class SearchParser {
 					//Invalid time format
 					else if (type == 2)
 						throw new IllegalArgumentException("Invalid time format!");
-					
+
 				}
 				else throw new IllegalArgumentException("Invalid parameter supplied: &7" + lastParam);
-				
+
 				paramSet = false;
 			}
 		}
-		
+
 		//Sort out locations
 		parseLocations();
-		
+
 	}
-	
+
 	/**
 	 * Formats min and max locations if the radius is set
 	 */
 	public void parseLocations() {
-		
+
+		// Check if there is a max radius
+		if (radius == null && Config.MaxRadius != 0) radius = Config.MaxRadius;
+
 		//If the radius is set we need to format the min and max locations
 		if (radius != null) {
-			
+
 			//Check if location and world are supplied
 			if (loc == null) loc = player.getLocation().toVector();
 			if (worlds == null) worlds = new String[]{ player.getWorld().getName() };
-			
+
 			//Format min and max
 			minLoc = new Vector(loc.getX() - radius, loc.getY() - radius, loc.getZ() - radius);
 			maxLoc = new Vector(loc.getX() + radius, loc.getY() + radius, loc.getZ() + radius);
-			
+
 		}
-		
+
 	}
 
 }
