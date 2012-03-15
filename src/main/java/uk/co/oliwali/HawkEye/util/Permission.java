2288e6a14abe8722c01d59298424183371ddfd5b
GoalieGuy6 <l.mikelonis@yahoo.com>
2012-03-15 07:51:18 -0400

    Add Vault permission support

--- src/main/java/uk/co/oliwali/HawkEye/util/Permission.java
+++ src/main/java/uk/co/oliwali/HawkEye/util/Permission.java
@@ -3,10 +3,11 @@ import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.RegisteredServiceProvider;
 
 import ru.tehkode.permissions.PermissionManager;
 import ru.tehkode.permissions.bukkit.PermissionsEx;
-
 import uk.co.oliwali.HawkEye.HawkEye;
 
 import com.nijiko.permissions.PermissionHandler;
@@ -18,33 +19,46 @@ import com.nijikokun.bukkit.Permissions.Permissions;
  * @author oliverw92
  */
 public class Permission {
-	
-	private HawkEye plugin;
+
+	private final HawkEye plugin;
 	private static PermissionPlugin handler = PermissionPlugin.BUKKITPERMS;
+	private static net.milkbowl.vault.permission.Permission vaultPermissions;
 	private static PermissionHandler permissionPlugin;
 	private static PermissionManager permissionsEx;
-	
+
 	/**
 	 * Check permissions plugins, deciding which one to use
 	 * @param instance
 	 */
 	public Permission(HawkEye instance) {
 		plugin = instance;
-		if (Bukkit.getServer().getPluginManager().isPluginEnabled("PermissionsEx")) {
+		PluginManager pm = Bukkit.getServer().getPluginManager();
+
+		if (pm.isPluginEnabled("Vault")) {
+			RegisteredServiceProvider<net.milkbowl.vault.permission.Permission> rsp = Bukkit.getServer().getServicesManager().getRegistration(net.milkbowl.vault.permission.Permission.class);
+			vaultPermissions = rsp.getProvider();
+			if (vaultPermissions != null) {
+				handler = PermissionPlugin.VAULT;
+				Util.info("Using " + vaultPermissions.getName() + " for user permissions");
+				return;
+			}
+		}
+
+		if (pm.isPluginEnabled("PermissionsEx")) {
         	handler = PermissionPlugin.PERMISSIONSEX;
         	permissionsEx = PermissionsEx.getPermissionManager();
         	Util.info("Using PermissionsEx for user permissions");
 		}
-        else if (Bukkit.getServer().getPluginManager().isPluginEnabled("Permissions")) {
+        else if (pm.isPluginEnabled("Permissions")) {
         	permissionPlugin = ((Permissions)plugin.getServer().getPluginManager().getPlugin("Permissions")).getHandler();
         	handler = PermissionPlugin.PERMISSIONS;
         	Util.info("Using Permissions for user permissions");
         }
         else {
-        	Util.info("No permission handler detected, only ops can use commands");
+        	Util.info("No permission handler detected, defaulting to superperms");
         }
 	}
-	
+
 	/**
 	 * Private method for checking a users permission level.
 	 * Permission checks from other classes should go through a separate method for each node.
@@ -59,6 +73,8 @@ public class Permission {
 		if (Config.OpPermissions && player.isOp())
 			return true;
 		switch (handler) {
+			case VAULT:
+				return vaultPermissions.has(player, node);
 			case PERMISSIONSEX:
 				return permissionsEx.has(player, node);
 			case PERMISSIONS:
@@ -68,7 +84,7 @@ public class Permission {
 		}
 		return false;
 	}
-	
+
 	/**
 	 * Permission to view different pages
 	 * @param player
@@ -77,7 +93,7 @@ public class Permission {
 	public static boolean page(CommandSender player) {
 		return hasPermission(player, "hawkeye.page");
 	}
-	
+
 	/**
 	 * Permission to search the logs
 	 * @param player
@@ -86,7 +102,7 @@ public class Permission {
 	public static boolean search(CommandSender player) {
 		return hasPermission(player, "hawkeye.search");
 	}
-	
+
 	/**
 	 * Permission to search a specific data type
 	 * @param player
@@ -95,7 +111,7 @@ public class Permission {
 	public static boolean searchType(CommandSender player, String type) {
 		return hasPermission(player, "hawkeye.search." + type.toLowerCase());
 	}
-	
+
 	/**
 	 * Permission to teleport to the location of a result
 	 * @param player
@@ -104,7 +120,7 @@ public class Permission {
 	public static boolean tpTo(CommandSender player) {
 		return hasPermission(player, "hawkeye.tpto");
 	}
-	
+
 	/**
 	 * Permission to use the rollback command
 	 * @param player
@@ -113,7 +129,7 @@ public class Permission {
 	public static boolean rollback(CommandSender player) {
 		return hasPermission(player, "hawkeye.rollback");
 	}
-	
+
 	/**
 	 * Permission to the hawkeye tool
 	 * @param player
@@ -122,7 +138,7 @@ public class Permission {
 	public static boolean tool(CommandSender player) {
 		return hasPermission(player, "hawkeye.tool");
 	}
-	
+
 	/**
 	 * Permission to be notified of rule breaks
 	 * @param player
@@ -131,7 +147,7 @@ public class Permission {
 	public static boolean notify(CommandSender player) {
 		return hasPermission(player, "hawkeye.notify");
 	}
-	
+
 	/**
 	 * Permission to preview rollbacks
 	 * @param player
@@ -140,7 +156,7 @@ public class Permission {
 	public static boolean preview(CommandSender player) {
 		return hasPermission(player, "hawkeye.preview");
 	}
-	
+
     /**
      * Permission to bind a tool
      * @param player
@@ -149,7 +165,7 @@ public class Permission {
 	public static boolean toolBind(CommandSender player) {
 		return hasPermission(player, "hawkeye.tool.bind");
 	}
-	
+
 	/**
 	 * Permission to rebuild
 	 * @param player
@@ -158,7 +174,7 @@ public class Permission {
 	public static boolean rebuild(CommandSender player) {
 		return hasPermission(player, "hawkeye.rebuild");
 	}
-	
+
 	/**
 	 * Check if a player is in a group
 	 * @param world
@@ -171,6 +187,8 @@ public class Permission {
 	}
 	public static boolean inGroup(String world, String player, String group) {
 		switch (handler) {
+			case VAULT:
+				return vaultPermissions.playerInGroup(world, player, group);
 			case PERMISSIONSEX:
 				return permissionsEx.getUser(player).inGroup(group);
 			case PERMISSIONS:
@@ -178,12 +196,13 @@ public class Permission {
 		}
 		return false;
 	}
-	
+
 	/**
 	 * Enumeration containing supported permission systems
 	 * @author oliverw92
 	 */
 	private enum PermissionPlugin {
+		VAULT,
 		PERMISSIONSEX,
 		PERMISSIONS,
 		BUKKITPERMS
